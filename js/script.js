26401207384903ed920f8a69d319b20ff792ebf2
Mohamad-Tanbari <124389317+Mohamad-Tanbari@users.noreply.github.com>
2023-03-07 18:56:04 +0000

    Attached javascript to button

--- js/script.js
+++ js/script.js
@@ -4,4 +4,5 @@
 // Created on: Sep 2023
 // This file contains the JS functions for index.html
 function buttonClicked() {
+  doccument.getElementById("page-content-answer").innerHTML = "Hello World";
 }
