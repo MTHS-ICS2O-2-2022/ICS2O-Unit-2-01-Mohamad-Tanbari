65540963fcc6d9db398fce40110c8b4a53a36b2b
Mohamad-Tanbari <124389317+Mohamad-Tanbari@users.noreply.github.com>
2023-03-07 18:45:34 +0000

    Built website using setup.py

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Mohamad
+// Created on: Sep 2023
+// This file contains the JS functions for index.html
+function buttonClicked() {
+}
